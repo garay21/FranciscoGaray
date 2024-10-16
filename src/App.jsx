c02cf7c972f12fecb9bd06f50226752996eee030
francisco <garaymolina21@gmail.com>
2024-10-16 19:12:35 -0300

    navegacion por paginas actualizada.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import React, { useState } from 'react';
 import Header from './view/Header';
 import Welcome from './view/Welcome'
-
+import Skills from './view/Skills';
+ 
 function App() {
+  const [currentView, setCurrentView] = useState('Home');
+
+  const NavPage = (view) => {
+    setCurrentView(view);
+  }
+
+  const RenderPage = () => {
+    switch(currentView){
+      case 'Home' : return <Welcome/>;
+      case 'Contact' : return <Welcome/>;
+      case 'Skills' : return <Skills/>;
+      case 'Projects' : return <Welcome/>;
+      case 'Education' : return <Welcome/>;
+      default : return <Welcome/>;
+    }
+  }
 
   return (
     <div className="container">
-        <Header/>
-        <Welcome/>
+        <Header NavPage={NavPage}/>
+        {
+          RenderPage()
+        }
     </div>      
   )
 }
--- src/view/Header.jsx
+++ src/view/Header.jsx
@@ -1,25 +1,27 @@
-const Header = () => {
+import '../styles/header.css'
+
+const Header = ({NavPage}) => {
     return (
         <header>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/mi-portfolio/">Home</a>
+                    <button className="nav-link" onClick={() => NavPage('Home')}>Home</button>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link" href="/mi-portfolio/Contact">Contacto</a>
+                                <button className="nav-link" onClick={()=>NavPage('Contact')}>Contacto</button>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/mi-portfolio/Skills">Conocimientos</a>
+                                <button className="nav-link" onClick={()=>NavPage('Skills')}>Conocimientos</button>
                             </li>    
                             <li className="nav-item">
-                                <a className="nav-link" href="/mi-portfolio/Projects">Proyectos</a>
+                                <button className="nav-link" onClick={()=>NavPage('Projects')}>Proyectos</button>
                             </li>  
                             <li className="nav-item">
-                                <a className="nav-link" href="/mi-portfolio/Education">Educacion</a>
+                                <button className="nav-link" onClick={()=>NavPage('Education')}>Educacion</button>
                             </li>                           
                         </ul>
                     </div>
