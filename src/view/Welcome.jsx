d3462d1c5417d869439c4d9c106590fa501e63d7
francisco <garaymolina21@gmail.com>
2024-10-16 21:10:58 -0300

    habilidades tecnicas funcionando cambio colores.

--- src/view/Welcome.jsx
+++ src/view/Welcome.jsx
@@ -3,12 +3,19 @@ import wsp from '../icons/whatsapp.png';
 import fcb from '../icons/facebook.png';
 import int from '../icons/instagram.png';
 import ltk from '../icons/linkedin.png';
+import { useState } from 'react';
 
 const Welcome = ()=>{
+    const [bgColor, setBgColor] = useState('');
+
+    const changeColor = (clr) => {
+        setBgColor(clr)
+    }
+
     return(
         <>
-            <div className="welcome d-flex justified-content-center align-items-center" >
-                <div className="text-center content-box">
+            <div className="welcome d-flex justified-content-center align-items-center" style={{backgroundColor: bgColor}}>
+                <div className="text-center content-box" >
                     <h1>Bienvenido !!, Soy Francisco Garay</h1>
                     <h3>-&gt;AnalistaProgramador();</h3>
                     <h4>desde Coquimbo, Chile. 🇨🇱</h4>
@@ -19,12 +26,12 @@ const Welcome = ()=>{
                 <div className='col-12'>
                     <h4 className="text-center">Habilidades Tecnicas.</h4>
                     <ul className='tecnical-container'>
-                        <li className='tecnical-button dwfs'>Dev. Web FullStack.</li>
-                        <li className='tecnical-button dsoft'>Dev. Software (DevOps).</li>
-                        <li className='tecnical-button dvvg'>Dev. VideoGame</li>
-                        <li className='tecnical-button abd'>Administración Base de Datos.</li>
-                        <li className='tecnical-button tcm'>Tecnico en Computacion.</li>
-                        <li className='tecnical-button wlm'>Windows y Linux manager.</li>
+                        <li className='tecnical-button dwfs' onMouseEnter={()=>changeColor('#20c0d8')}>Dev. Web FullStack.</li>
+                        <li className='tecnical-button dsoft'onMouseEnter={()=>changeColor('#efb630')}>Dev. Software (DevOps).</li>
+                        <li className='tecnical-button dvvg' onMouseEnter={()=>changeColor('#b8625c')}>Dev. VideoGame</li>
+                        <li className='tecnical-button abd' onMouseEnter={()=>changeColor('#576b3b')}>Administración Base de Datos.</li>
+                        <li className='tecnical-button tcm' onMouseEnter={()=>changeColor('#eb5148')}>Tecnico en Computacion.</li>
+                        <li className='tecnical-button wlm' onMouseEnter={()=>changeColor('#5d5f96')}>Windows y Linux manager.</li>
                     </ul>
                 </div>
                 <div className='col-12'>
