0fb4c98c2b24ebcafdf4631d5625d751a0422f17
francisco <garaymolina21@gmail.com>
2024-10-17 22:35:00 -0300

    contacto, correciones de diseño 02., mensaje succes o error rediseñado.

--- src/components/hooks/ItemContact.jsx
+++ src/components/hooks/ItemContact.jsx
@@ -1,7 +1,7 @@
 const ItemContact = ({Title,Ref,Description})=>{
     return(
         <li className="contact-item">
-            <label>{Title} : </label>
+            <label>{Title} - </label>
             <a href={Ref} target="_blank">{Description}</a>
         </li>
     );
